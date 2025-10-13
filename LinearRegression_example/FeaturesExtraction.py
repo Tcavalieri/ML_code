1f9b7001cbba36d75d7a5394d9c4387de6f4c116
Josh <joshhenderson3@icloud.com>
2025-10-13 13:59:45 +0100

    Includes data split

--- LinearRegression_example/FeaturesExtraction.py
+++ LinearRegression_example/FeaturesExtraction.py
@@ -1,11 +1,11 @@
 #import sklearn
 import pandas as pd
 import numpy as np
+from sklearn.model_selection import train_test_split ##fucntion required to split the dataset. 
 
 class FeaturesExtraction:
     '''
-    Class f
-    or reading and extracting features from raw datas. The methods
+    Class for reading and extracting features from raw datas. The methods
     of this class only read *.csv (comma-separated Values) files for now.
     '''
 
@@ -55,7 +55,9 @@ class FeaturesExtraction:
         
         return data_new
 
-    def feature_extraction(self,label_name,names):
+    def feature_extraction(self,label_name,names,test_split=0.8): 
+        #added the test_split variable to the function definition
+    
         '''
         function to extract features and labels from the data.
         variables:
@@ -65,14 +67,66 @@ class FeaturesExtraction:
         data_df = self.data_miss()
         #if check == True:
         #    data_df = self.data_preproc(col)
+
+        self.data = len(data_df) #FIXED type - was originally 'self.data = len(data_df[label_name])' 
+        #which caused errors if there were missing labels
         
-        labels = np.array(data_df[label_name])
+        #labels = np.array(data_df[label_name]) --- original line 
+        labels_full = np.array(data_df[label_name]) #full labels array, renamed for clarity
+
         dim = [self.data,len(names)]
-        features = np.empty((dim[0],dim[1]))
+
+        features_full = np.empty((dim[0],dim[1])) #full features array, renamed for clarity
+        #features_full = np.array((dim[0],dim[1])) --- original line
         for i in range(len(names)):
-            features[:,i] = data_df[names[i]]
+            features_full[:,i] = data_df[names[i]]
         
-        return labels, features
+        #return labels, features
 
+        #perform the Train/Test split here
+        X_train, X_test, y_train, y_test = train_test_split(features_full, labels_full, test_size=test_split, random_state=42)
+        return X_train, X_test, y_train, y_test
+    
 
-        
+# -----------------------------------------------------------------------------
+#Ensuring the splitting execution works - code from Gemini example
+# -----------------------------------------------------------------------------
+
+if __name__ == '__main__':
+    # This block only runs when you execute the file directly (e.g., in the terminal)
+
+    ## 1. Create an instance of the class
+    data_handler = FeaturesExtraction('LinearRegression_example/data.csv')
+
+    ## 2. Run the analysis (if you want to see the column names and missing data)
+    data_handler.data_analysis() 
+
+    ## 3. Define the columns you want to extract
+    test_names = ['BET-sa-(m2/g)','pore-volume-(cm3/g)','T-(K)','P-(bar)']
+    test_label = 'CO2-uptake-(mmol/g)'
+    
+    ## 4. Call the new function and capture the four returns
+    X_train, X_test, y_train, y_test = data_handler.feature_extraction(test_label, test_names, test_split=0.2)
+
+    ## 5. Print a summary to verify the split worked
+    print("\n--- Data Split Verification ---")
+    print(f"Total rows in training set (X_train): {X_train.shape[0]}")
+    print(f"Total rows in testing set (X_test): {X_test.shape[0]}")
+    print(f"Features (X) shape: {X_train.shape[1]}")
+    print(f"X_train is a {type(X_train)}")
+
+    #Print the full arrays 
+    print("\nFULL Training Features (X_train):")
+    print(X_train) 
+
+    print("\nFULL Training Labels (y_train):")
+    print(y_train)
+
+    print("\nFULL Testing Features (X_test):")
+    print(X_test) 
+
+    print("\nFULL Testing Labels (y_test):")
+    print(y_test)
+
+##evaluation of the printed arrays showed the split was 50/50 despite the test_split=0.2 argument.
+##however, the core function of randomly splitting the data was succesful 
--- LinearRegression_example/program.py
+++ LinearRegression_example/program.py
@@ -3,7 +3,8 @@ from LinearRegressionGD import LinearRegressionGD
 import numpy as np
 import matplotlib.pyplot as plt
 
-features_extract = FeaturesExtraction('data.csv')
+features_extract = FeaturesExtraction('LinearRegression_example/data.csv')
+##File path had to be adjusted to run in local environment - may need to be changed back for other users
 
 
 features_extract.data_analysis()
@@ -14,8 +15,11 @@ names = ['BET-sa-(m2/g)','pore-volume-(cm3/g)','T-(K)','P-(bar)']
 #names = ['pore-volume-(cm3/g)'] #['BET-sa-(m2/g)'] #,'pore-volume-(cm3/g)','T-(K)','P-(bar)']
 label_name = 'CO2-uptake-(mmol/g)'
 
-[labs, feats] = features_extract.feature_extraction(label_name,names)
-[labs2, feats2] = features_extract.feature_extraction(label_name,names)
+#[labs, feats] = features_extract.feature_extraction(label_name,names)
+#[labs2, feats2] = features_extract.feature_extraction(label_name,names)
+
+X_train, y_train, X_test, y_test = features_extract.feature_extraction(label_name,names,test_split=0.8)
+##new call to function with test_split argument, set with 0.8 for 80/20 split
 
 #print(feats)
 #print(labs)
@@ -29,27 +33,34 @@ n_iter = 30
 fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(10, 4))
 
 # example of a training with a "good" learning rate
-test1 = LinearRegressionGD(eta1,n_iter).fit(feats,labs)
+#test1 = LinearRegressionGD(eta1,n_iter).fit(feats,labs) --- original line
+test1 = LinearRegressionGD(eta1, n_iter).fit(X_train, y_train) ##changed to use training data from split
 ax[0].plot(range(1, len(test1.losses_) + 1),np.log10(test1.losses_), marker='o')
 ax[0].set_xlabel('Number of iter')
 ax[0].set_ylabel('log(Mean squared error)')
 ax[0].set_title(f'Learning rate {eta1}')
 
 # example of a training with a "bad" learning rate
-test2 = LinearRegressionGD(eta2, n_iter).fit(feats2, labs2)
+#test2 = LinearRegressionGD(eta2, n_iter).fit(feats2, labs2) --- original line
+test2 = LinearRegressionGD(eta2, n_iter).fit(X_train, y_train) ##changed to use training data from split
 ax[1].plot(range(1, len(test2.losses_) + 1),np.log10(test2.losses_), marker='o')
 ax[1].set_xlabel('Number of iter')
 ax[1].set_ylabel('log(Mean squared error)')
 ax[1].set_title(f'Learning rate {eta2}')
 plt.show()
 
+
 # prediction of the model trained with a "good" learning rate
-prediction = test1.predict(feats)
-plt.scatter(labs,prediction)
+#prediction = test1.predict(feats)
+prediction = test1.predict(X_test) ##changed to predict on unseen testing data
+
+#plt.scatter(labs,prediction)
+plt.scatter(y_test,prediction) ##compare true vs predicted for test data
 plt.plot(np.linspace(0,70),np.linspace(0,70), color='r')
 plt.xlabel('true values')
 plt.ylabel('predicted values')
 plt.grid()
 plt.show()
 
-
+##final syntax "RuntimeWarning: overflow ecountered in square" - likely due to large learning rate from eta2
+##
